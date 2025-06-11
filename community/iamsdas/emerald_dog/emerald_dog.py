aeb807c49e731bca5ed61f60145094dd129599e5
surya@fused.io <surya@fused.io>
2025-06-11 10:47:39 +0000

    another testing PR

--- community/iamsdas/emerald_dog/emerald_dog.py
+++ community/iamsdas/emerald_dog/emerald_dog.py
@@ -9,4 +9,5 @@ def udf(bounds: fused.types.Bounds = None, buffer_multiple: float = 1):
     tiles.geometry = tiles.buffer(buffer_multiple / (tiles.z.iloc[0]) ** 2)
     # Use print statements to debug
     print(f"{tiles.geometry.area.sample(3)=}")
+    
     return tiles
