3072cd1498dceb2295d802d8e14728e9aab71333
surya@fused.io <surya@fused.io>
2026-04-16 13:06:34 +0000

    preview_demo: 2026_04_16_1836

---
+++ public/preview_demo/widget_1/widget_1.py
@@ -0,0 +1,15 @@
+{
+  "type": "widget-builder",
+  "props": {
+    "defaultValue": {
+      "type": "slider",
+      "props": {
+        "label": "Value",
+        "param": "slider_value"
+      }
+    },
+    "showEditor": false,
+    "aiBuilderMode": "enabled",
+    "aiPanel": "right"
+  }
+}
